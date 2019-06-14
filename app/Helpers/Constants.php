2e3545c7ab98ff42210751d7bc5ea4c9065b2642
Ojo Oluwaseun Joseph <iamjoestack@gmail.com>
2019-06-14 11:39:28 +0100

    Added a custom helper to hold constants and other custom functions

---
+++ app/Helpers/Constants.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * Constants definations to be used in application
+ *
+ */
+
+# Pagination/List limit
+if(!defined('LIMIT_LIST'))
+	define('LIMIT_LIST', 10);
+
+# Constant for USERS_ENDPOINT
+
+if(!defined('USERS_ENDPOINT'))
+	define('USERS_ENDPOINT', "https://jsonplaceholder.typicode.com/users");
+
+
+# Constant for POSTS_ENDPOINT
+if(!defined('POSTS_ENDPOINT'))
+	define('POSTS_ENDPOINT', "https://jsonplaceholder.typicode.com/posts");
+
+# Constant for COMMENTS_ENDPOINT
+if(!defined('COMMENTS_ENDPOINT'))
+	define('COMMENTS_ENDPOINT', "https://jsonplaceholder.typicode.com/comments");

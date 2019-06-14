10e8bed00d4df48e1f8ceba4790cf982afc4bc50
Ojo Oluwaseun Joseph <iamjoestack@gmail.com>
2019-06-14 14:59:09 +0100

    Added tests for routes in 'web' middleware group

---
+++ tests/Feature/pagesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class pagesTest extends TestCase
+{
+
+    public function testPostsTest()
+    {
+        $response = $this->get('posts');
+
+        $response->assertStatus(200);
+    }
+
+    public function testPostSingleTest()
+    {
+        $response = $this->get('posts/1');
+
+        $response->assertStatus(200);
+    }
+
+
+    public function testUsersTest()
+    {
+        $response = $this->get('users');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUserSingleTest()
+    {
+        $response = $this->get('users/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function testPostWithCommentTest()
+    {
+        $response = $this->get('posts/1/comments');
+
+        $response->assertStatus(200);
+    }
+
+}
